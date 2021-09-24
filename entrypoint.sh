7a592f54e08ff9fd0b918d7370a907a464b5cbb3
Renee Cousins <nonarkitten@gmail.com>
2021-09-24 11:04:19 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ echo "Starting build on $time"
 pwd
 ls -al
 
-/opt/stm32cubeide/stm32cubeide -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -import Boards/MFS_F410CBU_A/ -build all
+/opt/stm32cubeide/stm32cubeide -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -import $1 -build all
