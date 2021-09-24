3769510dd146e1c1eed72d017e40de6dde372934
Renee Cousins <nonarkitten@gmail.com>
2021-09-24 13:07:10 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,3 @@ time=$(date)
 echo "Starting build on $time"
 
 /opt/stm32cubeide/stm32cubeide -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -import $1 -build all
-
-pwd
-ls -alR
